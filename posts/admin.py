7931e7e71d745bd97e522f3e25ba0e28b8a5bdad
MyCodeIsBadTrip <66428126+MyCodeIsBadTrip@users.noreply.github.com>
2020-09-10 18:05:11 +0300

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -8,4 +8,12 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("pub_date") 
     empty_value_display = "-пусто-"
 
+class GroupAdmin(admin.ModelAdmin): 
+    list_display = ("title", "slug", "description") 
+    search_fields = ("slug",) 
+    list_filter = ("slug",) 
+    empty_value_display = "-пусто-" 
+    
 admin.site.register(Post, PostAdmin) 
+
+admin.site.register(Group, GroupAdmin) 
