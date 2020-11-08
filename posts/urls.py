4a7e86b2b9eb357ea9e203334fa91da078b5aec9
Timofey <shorintim@yandex.ru>
2020-11-08 18:43:07 +0300

    ceka

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.index), 
-    path('user', views.account),
-    path('user/1', views.user_first),
-    path('user/<int:user-id>', views.user_page),
-    path('user/2', views.user_second),
-    path('user/login', views.login),
-    path('user/logout', views.logout),
+    path('', views.index),
+    path('/group/<slug>/', views.group_posts),
 ]
