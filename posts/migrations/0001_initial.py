080350dc8b546e4e2299385895feb1ca76d38e3a
Timofey <shorintim@yandex.ru>
2021-03-03 15:15:37 +0300

    пеп

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -17,20 +17,30 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Group',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('slug', models.SlugField(unique=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                serialize=False, verbose_name='ID')),
+                ('title', models.CharField(
+                max_length=200)),
+                ('slug', models.SlugField(
+                unique=True)),
                 ('description', models.TextField()),
             ],
         ),
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True,
+                serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
-                ('pub_date', models.DateTimeField(auto_now_add=True, verbose_name='date published')),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
-                ('group', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='group_posts', to='posts.Group')),
+                ('pub_date', models.DateTimeField(
+                auto_now_add=True, verbose_name='date published')),
+                ('author', models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                    related_name='posts', to=settings.AUTH_USER_MODEL)),
+                ('group', models.ForeignKey(
+                blank=True, null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                    related_name='group_posts', to='posts.Group')),
             ],
         ),
     ]
